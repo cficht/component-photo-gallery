cebe38565930870bfcd325a4a81d834bf6b4198b
cficht <chris.ficht@gmail.com>
2020-02-11 17:52:49 -0800

    basic react setup with data

---
+++ component-photo-gallery/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header.js';
+import ImageList from './ImageList.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ImageList />
+    </div>
+  );
+}
+
+export default App;
---
+++ component-photo-gallery/src/ImageItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class ImageItem extends Component {
+    render() {
+        return (
+            <div>
+                IMAGE ITEM
+            </div>
+        );
+    }
+}
